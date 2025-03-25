2726af3226ea38d0ddd0e56104ffaa40d2471a4a
Robert Hockley <robert.hockley@accenture.com>
2025-03-25 10:13:27 +0800

    HOO

--- Queues.tf
+++ Queues.tf
@@ -2,7 +2,7 @@ resource "aws_connect_queue" "queue1" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue1
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id = var.connect.outbound_caller_id_number_id
@@ -14,7 +14,7 @@ resource "aws_connect_queue" "queue2" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue2
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id = var.connect.outbound_caller_id_number_id
@@ -26,7 +26,7 @@ resource "aws_connect_queue" "queue3" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue3
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -38,7 +38,7 @@ resource "aws_connect_queue" "queue4" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue4
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -50,7 +50,7 @@ resource "aws_connect_queue" "queue5" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue5
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -62,7 +62,7 @@ resource "aws_connect_queue" "queue6" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue6
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -74,7 +74,7 @@ resource "aws_connect_queue" "queue7" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue7
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -86,7 +86,7 @@ resource "aws_connect_queue" "queue8" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue8
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -98,7 +98,7 @@ resource "aws_connect_queue" "queue9" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue9
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -110,7 +110,7 @@ resource "aws_connect_queue" "queue10" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue10
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -122,7 +122,7 @@ resource "aws_connect_queue" "queue11" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue11
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -134,7 +134,7 @@ resource "aws_connect_queue" "queue12" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue12
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -146,7 +146,7 @@ resource "aws_connect_queue" "queue13" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue13
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -158,7 +158,7 @@ resource "aws_connect_queue" "queue14" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue14
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -170,7 +170,7 @@ resource "aws_connect_queue" "queue15" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue15
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -182,7 +182,7 @@ resource "aws_connect_queue" "queue16" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue16
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -194,7 +194,7 @@ resource "aws_connect_queue" "queue17" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue17
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -206,7 +206,7 @@ resource "aws_connect_queue" "queue18" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue18
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -218,7 +218,7 @@ resource "aws_connect_queue" "queue19" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue19
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -230,7 +230,7 @@ resource "aws_connect_queue" "queue20" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue20
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
@@ -242,7 +242,7 @@ resource "aws_connect_queue" "queue21" {
   instance_id           = var.connect.instance_id
   name                  = var.connect.queue21
   description           = "Standard Queue"
-  hours_of_operation_id = var.conenct.hours_of_operation_id
+  hours_of_operation_id = var.connect.hours_of_operation_id
 
   outbound_caller_config {
     outbound_caller_id_number_id =var.connect.outbound_caller_id_number_id
